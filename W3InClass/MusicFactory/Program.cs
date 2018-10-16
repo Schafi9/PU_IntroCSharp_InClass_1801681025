c5973ce3c737baefb36b519de1770bbb6543ad77
plamiito0o <kristiqnged@gmail.com>
2018-10-16 15:42:01 +0300

    worked on my last program about music

--- W3InClass/MusicFactory/Program.cs
+++ W3InClass/MusicFactory/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,32 @@ namespace MusicFactory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Play some music...");
+            Console.WriteLine("Do you want to play some music?");
+            string answer=Console.ReadLine();
+
+            if (answer=="Yes")
+            {
+                //Console.WriteLine("Play music!");
+                using (SoundPlayer player = new SoundPlayer(@"C:\Users\fmi\Desktop\W3\PU_IntroCSharp_InClass_1801681025\
+                W3InClass\MusicFactory\Music\277751539693463.wav"))
+                {
+                    player.PlaySync();
+                }
+            }
+            else if(answer=="No")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else if (answer == "Fuck")
+            {
+                Console.WriteLine("Yes, fuck you too!!!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong input!");
+            }
+            Console.WriteLine($"My answer is: {answer}");
+            
         }
     }
 }
