70554a69ed6a47f24cc1ffa62803eda7daaf34df
simeonbb <centa2010@gmail.com>
2018-11-13 16:10:50 +0200

    tank

---
+++ W3InClass/PiggyBank/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PiggyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = 30;
+            int saving = 2;
+            int throwAwayForFalseVodka = 5;
+            int monthsInYear = 12;
+            double result = 0;
+            try
+            {
+                int tankPrice = int.Parse(Console.ReadLine());
+                int partyDays = int.Parse(Console.ReadLine());
+                
+                int daysWithoutVodka = days - partyDays;
+                int totalMonthSaaving = daysWithoutVodka * saving;
+                int netSaving = totalMonthSaaving - (throwAwayForFalseVodka * partyDays);
+                double monthForSaving4Tank = tankPrice / netSaving;
+                result = monthForSaving4Tank / monthsInYear;
+            }
+           
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please try again later!");
+                
+            }
+            result = Math.Round(result, 1, MidpointRounding.AwayFromZero);
+            //Console.WriteLine($"{result} years");
+            string months = result.ToString().Split(',')[1];
+            Console.WriteLine($"{(int)result} years {months}months!");
+
+        }
+    }
+}
