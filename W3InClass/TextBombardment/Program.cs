9a76975119221dee27523a9bc8c111cf09637633
simeonbb <centa2010@gmail.com>
2018-11-20 16:13:37 +0200

    wefsd

--- W3InClass/TextBombardment/Program.cs
+++ W3InClass/TextBombardment/Program.cs
@@ -12,22 +12,22 @@ namespace TextBombardment
             {
                 int lineWidth = int.Parse(Console.ReadLine());
                 string bombIndex = Console.ReadLine();
-                Console.WriteLine(sentence);
-                Console.WriteLine(lineWidth);
-                Console.WriteLine(bombIndex);
+                //Console.WriteLine(sentence);
+                //Console.WriteLine(lineWidth);
+                //Console.WriteLine(bombIndex);
 
                 double rows = sentence.Length / lineWidth;
-                if (rows%2==0)
-                {
+                //if (rows%2==0)
+                //{
                     for (int i = 0; i < sentence.Length; i++)
                     {
                         Console.Write(sentence[i]);
-                        if (sentence[i]==lineWidth)
+                        if (i==lineWidth)
                         {
                             Console.Write("\n");
                         }
                     }
-                }
+                //}
             }
             catch (Exception ex)
             {
